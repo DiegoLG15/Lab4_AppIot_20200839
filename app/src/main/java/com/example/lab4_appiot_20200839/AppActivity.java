f87e877c1e2e22d0dfaa677cb81870c1e2e43e10
DiegoLG15 <a20200839@pucp.edu.pe>
2023-10-01 20:00:01 -0500

    lab3 iot

--- app/src/main/java/com/example/lab4_appiot_20200839/AppActivity.java
+++ app/src/main/java/com/example/lab4_appiot_20200839/AppActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.lab4_appiot_20200839.databinding.ActivityAppBinding;
+import com.example.lab4_appiot_20200839.services.TypicodeServices;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class AppActivity extends AppCompatActivity {
 
+    TypicodeServices typicodeService;
+    ActivityAppBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app);
+        binding = ActivityAppBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        typicodeService = new Retrofit.Builder()
+                .baseUrl("https://randomuser.me")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(TypicodeServices.class);
+
     }
 }
